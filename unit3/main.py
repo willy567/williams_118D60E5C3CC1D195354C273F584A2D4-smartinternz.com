773b1d704431bd0361f814bc14baa17fafbd4b2c
willy567 <150062718+willy567@users.noreply.github.com>
2023-11-06 22:21:23 +0530

    Add files via upload

---
+++ unit3/main.py
@@ -0,0 +1,13 @@
+def linear_search_product(product_list, target_product):
+  indices = []
+
+  for i, product in enumerate(product_list):
+      if product == target_product:
+          indices.append(i)
+
+  return indices
+products = ["apple", "banana", "orange", "apple", "grape"]
+target = "apple"
+
+result = linear_search_product(products, target)
+print(f"Indices of '{target}': {result}")
